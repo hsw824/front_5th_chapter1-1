9cf928f1cff82e8be0cd4e054fe9ee2d032e1b5e
hsw824 <ghdtjddn0824@naver.com>
2025-03-28 01:49:09 +0900

    feat: base url 설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  base: "/front_5th_chapter1-1/",
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-  },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    base: isProduction ? "/front_5th_chapter1-1/" : "/",
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: "index.html",
+          hash: "index.hash.html",
+        },
+      },
+    },
+  };
 });
